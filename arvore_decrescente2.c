0a155bea8b4e0efa8933236a0543bec0bc128f9f
mariamamoura <mouramariama@gmail.com>
2018-10-16 15:14:33 -0300

    V.0.2
    Alteração no main()

---
+++ arvore_decrescente2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct folha{
+    int valor;
+    struct folha *esq;
+    struct folha *dir;
+};
+
+void inserir(struct folha **arvore, int valor){
+    struct folha *atual = *arvore;
+    if (atual == NULL){
+        struct folha *nova_folha = malloc(sizeof(struct folha));
+        nova_folha -> valor = valor;
+        nova_folha -> esq = NULL;
+        nova_folha -> dir = NULL;
+
+        *arvore = nova_folha;
+    }
+}
+
+void imprimir (struct folha *arvore){
+    struct folha* atual = arvore;
+    if (atual != NULL){
+        imprimir(atual -> dir);
+        printf("%d", atual -> valor);
+        imprimir(atual -> esq);
+    }
+}
+
+int tamanho(struct folha *arvore){
+    int contador = 0;
+    struct folha *atual = arvore;
+    if (tamanho != NULL){
+        contador += tamanho(atual -> esq);
+        contador++;
+        contador += tamanho (atual -> dir);
+    } return contador;
+}
+
+int main(){
+    struct folha* raiz = NULL;
+
+    int valores;
+    do{
+        scanf("%d", valores);
+        inserir(&raiz, valores);
+    } while (valores != NULL);
+
+    imprimir(raiz);
+    printf("tamanho:%d", tamanho(raiz));
+    return(EXIT_SUCCESS);
+
+
+    return 0;
+}
