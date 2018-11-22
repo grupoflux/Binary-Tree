9726c7b9d4ca86ea64f6af5228a9c3104cdef66e
ASF <35273424+Snakedark@users.noreply.github.com>
2018-11-22 17:18:36 -0300

    lendo dados do banco de dados (#7)
    * lendo dados do banco de dados
    
    * falta quebrar os dados no arquivo
    
    * falta separar dados no csv
    
    * falta separar dados no csv

---
+++ https_get.c
@@ -0,0 +1,52 @@
+# include <stdio.h>
+# include <string.h>
+# include <curl/curl.h>
+
+
+int main() {
+
+    char stream[1024], *virgula;
+
+    CURL *curl;
+    CURLcode res;
+
+    FILE *dados;
+
+    
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+
+    dados = fopen("teste.csv","a+");
+
+    if (curl) {
+        
+        curl_easy_setopt(curl, CURLOPT_URL, "https://m9963ywzsg.execute-api.us-east-1.amazonaws.com/default/queryInHouse?nome=Gustavo");
+        
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, dados);
+
+        #ifdef SKIP_PEER_VERIFICATION
+            curl_easy_setopt(curl, CURLOPT_SSL_VERFYPEER, 0L);
+        #endif
+
+        #ifdef SKIP_HOSTNAME_VERIFICATION
+            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+        #endif
+
+        res = curl_easy_perform(curl);
+
+        fprintf(dados, "%s\n");
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        
+        curl_easy_cleanup(curl);
+    }
+
+    printf("\n");
+    curl_global_cleanup();
+
+    fclose(dados);    
+
+    return 0; 
+}
